cb22a0c472cde75620f591361ff43042b6424ca8
Edoardo Benali <edoardo.benali@gmail.com>
2024-07-17 07:28:52 +0100

    commented out transparent bg

--- init.lua
+++ init.lua
@@ -3,5 +3,5 @@ require("config.lazy")
 
 vim.cmd [[colorscheme moonfly]]
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+--[[ vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"}) ]]--
